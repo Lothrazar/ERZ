870de8a1f56ddf4747591b2ab73698a48927b296
sam <samson.bassett@gmail.com>
2017-08-03 09:15:02 -0700

    rm more unused entities

--- src/main/java/teamroots/emberroot/RegistryManager.java
+++ src/main/java/teamroots/emberroot/RegistryManager.java
@@ -55,19 +55,19 @@ public class RegistryManager {
  
 		int id = 0;
 		//EntityRegistry.registerModEntity(new ResourceLocation(Const.MODID,"auspicious_point"), EntityAuspiciousPoint.class, "auspicious_point", id ++, Roots.instance, 64, 20, true);
-		EntityRegistry.registerModEntity(new ResourceLocation(Const.MODID,"fire_jet"), EntityFireJet.class, "fire_jet", id ++, Roots.instance, 64, 20, true); 
+	//	EntityRegistry.registerModEntity(new ResourceLocation(Const.MODID,"fire_jet"), EntityFireJet.class, "fire_jet", id ++, Roots.instance, 64, 20, true); 
 //		EntityRegistry.registerModEntity(new ResourceLocation(Const.MODID,"petal_shell"), EntityPetalShell.class, "petal_shell", id ++, Roots.instance, 64, 1, true); 
-		EntityRegistry.registerModEntity(new ResourceLocation(Const.MODID,"boost"), EntityBoost.class, "boost", id ++, Roots.instance, 64, 1, true);
+		//EntityRegistry.registerModEntity(new ResourceLocation(Const.MODID,"boost"), EntityBoost.class, "boost", id ++, Roots.instance, 64, 1, true);
 		EntityRegistry.registerModEntity(new ResourceLocation(Const.MODID,"deer"), EntityDeer.class, "deer", id ++, Roots.instance, 64, 1, true);
 		EntityRegistry.registerEgg(      new ResourceLocation(Const.MODID,"deer"), Misc.intColor(161, 132, 88), Misc.intColor(94, 77, 51)); 
-		EntityRegistry.registerModEntity(new ResourceLocation(Const.MODID,"flare"), EntityFlare.class, "flare", id ++, Roots.instance, 64, 1, true); 
+		//EntityRegistry.registerModEntity(new ResourceLocation(Const.MODID,"flare"), EntityFlare.class, "flare", id ++, Roots.instance, 64, 1, true); 
 		EntityRegistry.registerModEntity(new ResourceLocation(Const.MODID,"sprout"), EntitySprout.class, "sprout", id ++, Roots.instance, 64, 1, true);
 		EntityRegistry.registerEgg(      new ResourceLocation(Const.MODID,"sprout"), Misc.intColor(136, 191, 33), Misc.intColor(165, 232, 42));
 		//EntityRegistry.registerModEntity(new ResourceLocation(Const.MODID,"barrow"), EntityBarrow.class, "barrow", id ++, Roots.instance, 64, 20, true); 
 		EntityRegistry.registerModEntity(new ResourceLocation(Const.MODID,"fairy"), EntityFairy.class, "fairy", id ++, Roots.instance, 64, 1, true);
 		EntityRegistry.registerEgg(      new ResourceLocation(Const.MODID,"fairy"), Misc.intColor(255, 214, 255), Misc.intColor(209, 255, 173));
 		EntityRegistry.registerModEntity(new ResourceLocation(Const.MODID,"fairy_circle"), EntityFairyCircle.class, "fairy_circle", id ++, Roots.instance, 64, 20, true);
-		EntityRegistry.registerModEntity(new ResourceLocation(Const.MODID,"blink_projectile"), EntityBlinkProjectile.class, "blink_projectile", id ++, Roots.instance, 64, 1, true);
+	//	EntityRegistry.registerModEntity(new ResourceLocation(Const.MODID,"blink_projectile"), EntityBlinkProjectile.class, "blink_projectile", id ++, Roots.instance, 64, 1, true);
 		
 		List<BiomeEntry> biomeEntries = new ArrayList<BiomeEntry>();
 		biomeEntries.addAll(BiomeManager.getBiomes(BiomeType.COOL));
@@ -92,19 +92,19 @@ public class RegistryManager {
 	@SubscribeEvent
     public void registerRendering(ModelRegistryEvent event){
   
-		RenderingRegistry.registerEntityRenderingHandler(EntityFireJet.class, new RenderNull.Factory());
+		//RenderingRegistry.registerEntityRenderingHandler(EntityFireJet.class, new RenderNull.Factory());
  
-		RenderingRegistry.registerEntityRenderingHandler(EntityBoost.class, new RenderNull.Factory());
+		//RenderingRegistry.registerEntityRenderingHandler(EntityBoost.class, new RenderNull.Factory());
 		RenderingRegistry.registerEntityRenderingHandler(EntityDeer.class, new RenderDeer.Factory());
  
-		RenderingRegistry.registerEntityRenderingHandler(EntityFlare.class, new RenderNull.Factory());
+	//	RenderingRegistry.registerEntityRenderingHandler(EntityFlare.class, new RenderNull.Factory());
 
 		RenderingRegistry.registerEntityRenderingHandler(EntitySprout.class, new RenderSprout.Factory());
 	//	RenderingRegistry.registerEntityRenderingHandler(EntityBarrow.class, new RenderNull.Factory());
 
 		RenderingRegistry.registerEntityRenderingHandler(EntityFairy.class, new RenderFairy.Factory());
-		RenderingRegistry.registerEntityRenderingHandler(EntityFairyCircle.class, new RenderNull.Factory());
-		RenderingRegistry.registerEntityRenderingHandler(EntityBlinkProjectile.class, new RenderNull.Factory());
+		//RenderingRegistry.registerEntityRenderingHandler(EntityFairyCircle.class, new RenderNull.Factory());
+		//RenderingRegistry.registerEntityRenderingHandler(EntityBlinkProjectile.class, new RenderNull.Factory());
 		
  
 	}
