a47f79f981dab35ace26ab02049ade6d5d7895a0
sam <samson.bassett@gmail.com>
2017-08-16 16:48:48 -0700

    change default spawns

--- src/main/java/teamroots/emberroot/config/ConfigManager.java
+++ src/main/java/teamroots/emberroot/config/ConfigManager.java
@@ -37,10 +37,10 @@ public class ConfigManager {
     //spawn is min,max,weight.      props is health, attack, speed, followRange
     //-1 means unlisted from config
     //?? is this true? # Attack damage of Enderminies. 7=Enderman damage, 3=Zombie damage
-    entityConfigs.add(EntityWitherCat.config.setDefaultSpawns(0, 0, 0).setDefaultProperties(20, 2, 32));
+    entityConfigs.add(EntityWitherCat.config.setDefaultSpawns(1, 1, 1).setDefaultProperties(20, 2, 32));
     entityConfigs.add(EntityConcussionCreeper.config.setDefaultSpawns(1, 1, 17).setDefaultProperties(20, -1, 32));
     entityConfigs.add(EntityDeer.config.setDefaultSpawns(3, 7, 20).setDefaultProperties(15, -1, 32));
-    entityConfigs.add(EntityDireSlime.config.setDefaultSpawns(1, 1, 17).setDefaultProperties(12, -1, 32));//0.3F,
+    entityConfigs.add(EntityDireSlime.config.setDefaultSpawns(1, 1, 12).setDefaultProperties(12, -1, 32));//0.3F,
     entityConfigs.add(EntityEnderminy.config.setDefaultSpawns(1, 1, 17).setDefaultProperties(15, 10, 32));//0.3F,
     entityConfigs.add(EntityFairy.config.setDefaultSpawns(5, 10, 20).setDefaultProperties(18, -1, 16));
     entityConfigs.add(EntityAncientGolem.config.setDefaultSpawns(1, 1, 15).setDefaultProperties(25, 5, 16));
@@ -50,17 +50,17 @@ public class ConfigManager {
     entityConfigs.add(EntityWitherWitch.config.setDefaultSpawns(1, 1, 15).setDefaultProperties(30, -1, 16));
     entityConfigs.add(EntityOwl.config.setDefaultSpawns(1, 1, 15).setDefaultProperties(10, -1, -1));
     entityConfigs.add(EntityFallenMount.config.setDefaultSpawns(0, 0, 0).setDefaultProperties(30, -1, -1));
-    entityConfigs.add(EntityFallenKnight.config.setDefaultSpawns(1, 1, 15).setDefaultProperties(20, 4, 32));
+    entityConfigs.add(EntityFallenKnight.config.setDefaultSpawns(1, 3, 20).setDefaultProperties(20, 4, 32));
     entityConfigs.add(EntitySprout.config.setDefaultSpawns(2, 4, 1).setDefaultProperties(6, 0, 32));//0.2F,
     //roots1 direwolf
     entityConfigs.add(EntityTimberWolf.config.setDefaultSpawns(2, 5, 10).setDefaultProperties(32, 4, 32).setDefaultBiome(false, new String[] { "minecraft:ice_flats", "minecraft:extreme_hills", "minecraft:savanna", "minecraft:mesa", "minecraft:roofed_forest", "minecraft:taiga", "minecraft:taiga_cold", "minecraft:ice_mountains" }));
     //smallest ones get a bit of a wider spawn
-    entityConfigs.add(EntitySpriteling.config.setDefaultSpawns(4, 6, 2).setDefaultProperties(16, 4, 32).setDefaultBiome(false, new String[] { "minecraft:hell", "minecraft:sky", "minecraft:jungle_hills" }));
+    entityConfigs.add(EntitySpriteling.config.setDefaultSpawns(1, 4, 2).setDefaultProperties(16, 4, 32).setDefaultBiome(false, new String[] { "minecraft:hell", "minecraft:sky", "minecraft:jungle_hills" }));
     //these babies only spawn in end
     entityConfigs.add(EntitySprite.config.setDefaultSpawns(1, 3, 5).setDefaultProperties(40, 6, 32)
         .setDefaultBiome(false, new String[] { "minecraft:ice_mountains", "minecraft:taiga", "minecraft:ice_mountains", "minecraft:ice_flats", "minecraft:frozen_river", "minecraft:frozen_ocean", "minecraft:taiga_cold", "minecraft:taiga_cold_hills", "minecraft:mutated_ice_flats", "minecraft:mutated_taiga_cold"
         }));
-    entityConfigs.add(EntityGreaterSprite.config.setDefaultSpawns(1, 3, 2).setDefaultProperties(80, 6, 32).setDefaultBiome(false, new String[] { "minecraft:sky" }));
+    entityConfigs.add(EntityGreaterSprite.config.setDefaultSpawns(1, 1, 1).setDefaultProperties(80, 6, 32).setDefaultBiome(false, new String[] { "minecraft:sky" }));
     entityConfigs.add(EntityFrozenKnight.config.setDefaultSpawns(1, 1, 15).setDefaultProperties(20, 4, 32)
         .setDefaultBiome(false, new String[] { "minecraft:sky" }));
     load();
